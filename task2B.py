9cefd150de1aa1f9430b240d927e55b45367a939
fb530 <77330186+fb530@users.noreply.github.com>
2021-01-27 09:05:06 +0000

    Add files via upload

--- task2B.py
+++ task2B.py
@@ -1,6 +1,7 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
 
+
 def run():
     stations = build_station_list()
     update_water_levels(stations)
@@ -8,6 +9,7 @@ def run():
         my_list = stations_level_over_threshold(stations, 0.8)
     for i in my_list:
         "return in the form: name level"
-        print(str(i[0])+" "+str(i[1]))
+        print(str(i[0]) + " " + str(i[1]))
+
 
 run()
