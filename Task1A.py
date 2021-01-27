df10602c7e4d403b77cbe83053afbb868f793ea4
David Hao <hydhyd2002@vip.sina.com>
2021-01-27 00:04:27 +0000

    Test Git Commit and Push Functionality in Task 1A

--- Task1A.py
+++ Task1A.py
@@ -23,5 +23,5 @@ def run():
 
 
 if __name__ == "__main__":
-    print("*** Task 1A: CUED Part IA Flood Warning System ***")
+    print("*** Task 1A: CUED Part IA Flood Warning System Test (Git Functionality Test)***")
     run()
