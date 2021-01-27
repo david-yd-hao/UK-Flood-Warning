ad767140662778e7a10251c82c0bb205a6979458
fb530 <77330186+fb530@users.noreply.github.com>
2021-01-27 08:21:21 +0000

    Add files via upload

--- Task1B.py
+++ Task1B.py
@@ -3,10 +3,10 @@ from floodsystem.geo import stations_by_distance
 
 
 def run():
-    
+
     My_list = []
     stations = build_station_list()
-    list_tuple =[]
+    list_tuple = []
     stations_2 = stations_by_distance(stations, (52.2053, 0.1218))
     for station_tuple in stations_2:
         list_tuple.append((station_tuple[0].name, station_tuple[0].town, station_tuple[1]))
@@ -15,6 +15,6 @@ def run():
     My_list.append(list_tuple[-10:])
 
     print(My_list)
-    
-run()
-    
+
+
+run() 
