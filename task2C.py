26d4a08a19db35376fe426000192cf07813dca36
fb530 <77330186+fb530@users.noreply.github.com>
2021-01-27 09:20:39 +0000

    Add files via upload

--- task2C.py
+++ task2C.py
@@ -1,12 +1,12 @@
 from floodsystem.stationdata import build_station_list, update_water_levels
-from floodsystem.flood import stations_level_over_threshold, stations_highest_rel_level
+from floodsystem.flood import stations_highest_rel_level
 
 
 def run():
     stations = build_station_list()
     update_water_levels(stations)
     """create list of ten stations with higher latest level"""
-    list4 = stations_highest_rel_level(stations, 12) 
+    list4 = stations_highest_rel_level(stations, 12)
     """12 is used instead of 12 to account for the repeated stations which have to be removed"""
     for i in range(len(list4)):
         """use this piece of code to avoid stations with multiple latest levels (Hexham is an example)"""
