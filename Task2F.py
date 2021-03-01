8ca026f1f41b29d4c6fda55534f0e20219f4158a
David Hao <hydhyd2002@vip.sina.com>
2021-03-01 04:05:20 +0000

    Finished all

--- Task2F.py
+++ Task2F.py
@@ -1,6 +1,6 @@
 from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.plot import plot_water_level_with_fit
-from datetime import datetime, timedelta
+from datetime import timedelta
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_highest_rel_level
 
